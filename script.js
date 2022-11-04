cc6c1bc9898a512b90b75cddbcc99660a3168c26
BenOlm <benmingham@gmail.com>
2022-11-04 22:22:37 +1300

    responsive layout

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 document
   .querySelector(`.calculate-button`)
   .addEventListener("click", function () {
-    console.log(`test`);
-    // Code to execute goes here
+    // Grabs height and weight variables from the two input fields
     const height = Number(document.querySelector(`.height-input`).value);
-    console.log(`The height is ${height} cm`);
 
     const weight = Number(document.querySelector(`.weight-input`).value);
-    console.log(`The weight is ${weight} kg`);
 
+    // Calculates the BMI from the two vaiables
+    // Fixed the length of the result to 2 decimal places
     const BMI = (weight / height ** 2).toFixed(2);
-    console.log(BMI);
 
+    // Inserts the result onto the page
     document.querySelector(`.BMI`).textContent = `${BMI} kg/m2`;
   });
